d226babc522ff0a4d23577eef88dd7535def412b
Armand <armand.robin@capgemini.com>
2020-10-04 16:26:36 +0200

    V00.00.02

--- app/src/main/res/raw/data.sql
+++ app/src/main/res/raw/data.sql
@@ -1,1 +1,2 @@
-INSERT INTO Administration (id_administration,libelle, valeur) VALUES (1,"VERSION", "V00.00.01");
+DELETE Administration WHERE libelle="VERSION";
+INSERT INTO Administration (id_administration,libelle, valeur) VALUES (1,"VERSION", "V00.00.02");
