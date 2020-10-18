d6916430e30070bd72d2642db777682ddcba1c71
Armand <armand.robin@capgemini.com>
2020-10-18 22:45:46 +0200

    V00.05.00

--- app/src/main/java/fr/robin/android/surtaincom/ui/compteRendu/CompteRenduAdapter.java
+++ app/src/main/java/fr/robin/android/surtaincom/ui/compteRendu/CompteRenduAdapter.java
@@ -1,11 +1,16 @@
 package fr.robin.android.surtaincom.ui.compteRendu;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.core.text.HtmlCompat;
@@ -14,13 +19,14 @@ import java.util.HashMap;
 import java.util.List;
 
 import fr.robin.android.surtaincom.R;
+import fr.robin.android.surtaincom.data.Cache;
 import fr.robin.android.surtaincom.models.bo.Article;
 import fr.robin.android.surtaincom.util.Data;
 
 public class CompteRenduAdapter extends ArrayAdapter<Article> {
 
-    public CompteRenduAdapter(Context context, List<Article> Articles) {
-            super(context, 0, Articles);
+    public CompteRenduAdapter(Context context, List<Article> articles) {
+            super(context, 0, articles);
     }
 
     @Override
@@ -34,15 +40,36 @@ public class CompteRenduAdapter extends ArrayAdapter<Article> {
                 viewHolder = new CompteRenduViewHolder();
                 viewHolder.titre = (TextView) convertView.findViewById(R.id.textViewTitre);
                 viewHolder.corps = (TextView) convertView.findViewById(R.id.textViewCorps);
+                viewHolder.bouton = (Button) convertView.findViewById(R.id.button);
                 convertView.setTag(viewHolder);
             }
             //Article
             Article article = getItem(position);
             HashMap infos = Data.getData(article.getCorps());
             //il ne reste plus qu'à remplir notre vue
-            viewHolder.titre.setText(Html.fromHtml((String)infos.get("TITRE"), HtmlCompat.FROM_HTML_MODE_LEGACY));
-            viewHolder.corps.setText(Html.fromHtml((String)infos.get("CONTENU"),HtmlCompat.FROM_HTML_MODE_LEGACY));
+            viewHolder.bouton.setText(Html.fromHtml((String)infos.get("TITRE"), HtmlCompat.FROM_HTML_MODE_LEGACY));
+            //URL PDF
+            String url = Cache.siteClient.getUrl()+(String)infos.get("CONTENU");
+            //Bouton
+            setOnClick( viewHolder.bouton,url);
             //
             return convertView;
+    }
+
+    private void setOnClick(final Button btn, final String url){
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setDataAndType(Uri.parse(url), "application/pdf");
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                Intent newIntent = Intent.createChooser(intent, "Open File");
+                try {
+                    v.getContext().startActivity(newIntent);
+                } catch (ActivityNotFoundException e) {
+                    // Instruct the user to install a PDF reader here, or something
+                }
+            }
+        });
     }
 }
--- app/src/main/java/fr/robin/android/surtaincom/ui/compteRendu/CompteRenduViewHolder.java
+++ app/src/main/java/fr/robin/android/surtaincom/ui/compteRendu/CompteRenduViewHolder.java
@@ -1,8 +1,11 @@
 package fr.robin.android.surtaincom.ui.compteRendu;
 
+import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class CompteRenduViewHolder {
     public TextView titre;
     public TextView corps;
+    public Button bouton;
 }
