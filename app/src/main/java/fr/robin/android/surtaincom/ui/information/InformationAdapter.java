26b94d3a82239119cf0b391901fb1dddd1b469ad
Armand <armand.robin@capgemini.com>
2020-10-31 15:56:18 +0100

    V00.09.00

--- app/src/main/java/fr/robin/android/surtaincom/ui/information/InformationAdapter.java
+++ app/src/main/java/fr/robin/android/surtaincom/ui/information/InformationAdapter.java
@@ -1,11 +1,14 @@
 package fr.robin.android.surtaincom.ui.information;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.core.text.HtmlCompat;
@@ -14,6 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import fr.robin.android.surtaincom.R;
+import fr.robin.android.surtaincom.data.Cache;
 import fr.robin.android.surtaincom.models.bo.Article;
 import fr.robin.android.surtaincom.util.Data;
 
@@ -33,20 +37,33 @@ public class InformationAdapter extends ArrayAdapter<Article> {
             if(viewHolder == null){
                 viewHolder = new InformationViewHolder();
                 viewHolder.titre = (TextView) convertView.findViewById(R.id.textViewTitre);
-                viewHolder.corps = (TextView) convertView.findViewById(R.id.textViewCorps);
+                viewHolder.corps = (Button) convertView.findViewById(R.id.buttonURL);
                 convertView.setTag(viewHolder);
             }
             //Article
             Article article = getItem(position);
             HashMap infos = Data.getData(article.getCorps());
+            String titre = (String)infos.get("TITRE");
+            String contenu = (String)infos.get("CONTENU");
+        String url = (String)infos.get("ARTICLE");
             //il ne reste plus qu'à remplir notre vue
-            viewHolder.titre.setText(Html.fromHtml((String)infos.get("TITRE"), HtmlCompat.FROM_HTML_MODE_LEGACY));
-            viewHolder.corps.setText(Html.fromHtml((String)infos.get("CONTENU"),HtmlCompat.FROM_HTML_MODE_LEGACY));
-            String url = (String)infos.get("ARTICLE");
-            if(url != null){
-                url = "";
+            if(titre != null) {
+                viewHolder.titre.setText(Html.fromHtml(titre, HtmlCompat.FROM_HTML_MODE_LEGACY));
             }
-            //
-            return convertView;
+        if(contenu != null) {
+            viewHolder.corps.setText(Html.fromHtml(contenu, HtmlCompat.FROM_HTML_MODE_LEGACY));
+        }
+        if(url != null) {
+            viewHolder.corps.setTag(Cache.siteClient.getUrl()+url);
+            viewHolder.corps.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View arg0) {
+                        String url = (String)arg0.getTag();
+                        Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse(url));
+                        getContext().startActivity(viewIntent);
+                    }
+                });
+        }
+        //
+        return convertView;
     }
 }
